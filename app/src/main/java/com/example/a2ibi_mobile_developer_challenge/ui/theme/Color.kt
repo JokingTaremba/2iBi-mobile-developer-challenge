0ebd3751a44a98d3aa51a368350194f06ba36bb8
JokingTaremba <jokingtaremba@gmail.com>
2025-08-14 09:59:34 +0200

    feat: First configurations

---
+++ app/src/main/java/com/example/a2ibi_mobile_developer_challenge/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.a2ibi_mobile_developer_challenge.ui.theme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val backgroundGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0x42424242),
+        Color(0x33333333)
+    ),
+    startY = 0f,
+    endY = Float.POSITIVE_INFINITY
+)
---
+++ app/src/main/java/com/example/a2ibi_mobile_developer_challenge/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.example.a2ibi_mobile_developer_challenge.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+@Composable
+fun _2iBimobiledeveloperchallengeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = DarkColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
